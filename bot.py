4e6711f76f44dc4dd7a5cd61b01a5c06b0068aad
matteopalmieri <matteopalmieri2000@gmail.com>
2021-09-19 15:29:48 +0200

    boh

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-
 def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
+    print(1)
 
 updater = Updater('YOUR TOKEN HERE')
 
